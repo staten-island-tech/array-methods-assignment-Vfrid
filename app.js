5f1b4af213cd0ca639572f1c602b14ddaf586150
Vfrid <113192881+Vfrid@users.noreply.github.com>
2023-10-04 10:16:26 -0400

    fdsafdsa

---
+++ app.js
@@ -0,0 +1,3 @@
+const class_averages = [{class: "APWH", average:""},{class: "APCSP", average:""},{class: "AP Physics 1", average:""}];
+// const students = [{name: "Jotham", apwh_av:"55", apcsp_av:"67", apphys_av:"43"},{name: "Yan", apwh_av:"84", apcsp_av:"90", apphys_av:"92"},
+// {name: "Veronica", apwh_av:"100", apcsp_av:"100", apphys_av:"100"},{name: "Simon", apwh_av:"96", apcsp_av:"0", apphys_av:"92"}];
